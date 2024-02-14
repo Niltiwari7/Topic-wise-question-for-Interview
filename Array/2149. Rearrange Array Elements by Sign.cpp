df3c083412fcf3c58e60cc85168c947ac40cf828
Niltiwari7 <nt465638@gmail.com>
2024-02-14 06:18:39 +0530

    Iteration of array, easy

---
+++ Array/2149. Rearrange Array Elements by Sign.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>neg;
+        vector<int>pos;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<0){
+                neg.push_back(nums[i]);
+            }else{
+                pos.push_back(nums[i]);
+            }
+        }
+        vector<int>ans;
+        for(int i =0;i<neg.size();i++){
+            ans.push_back(pos[i]);
+            ans.push_back(neg[i]);
+        }
+        return ans;
+    }
+};
