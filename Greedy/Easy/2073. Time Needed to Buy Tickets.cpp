62c63be0dd33c9a2c3b1907a27156786ae6bdd27
Niltiwari7 <nt465638@gmail.com>
2024-04-09 05:48:33 +0530

    easy greedy

---
+++ Greedy/Easy/2073. Time Needed to Buy Tickets.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans = 0;
+        while(true){
+            for(int i = 0;i<tickets.size();i++){
+                if(tickets[i]==0)continue;
+                tickets[i]--;
+                ans++;
+                if(tickets[k]==0)return ans;
+            }           
+        }
+        return ans;
+    }
+};
