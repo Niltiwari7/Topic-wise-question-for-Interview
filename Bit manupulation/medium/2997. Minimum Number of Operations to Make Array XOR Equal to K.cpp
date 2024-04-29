097e1537a212d59f70c1e30e4ad807c14e698a64
Niltiwari7 <nt465638@gmail.com>
2024-04-30 04:07:10 +0530

    Bit mainupulation, simpler observation, xor property

---
+++ Bit manupulation/medium/2997. Minimum Number of Operations to Make Array XOR Equal to K.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        for(auto num:nums)
+        {
+            k^=num;
+        }
+        return __builtin_popcount(k);
+    }
+};
