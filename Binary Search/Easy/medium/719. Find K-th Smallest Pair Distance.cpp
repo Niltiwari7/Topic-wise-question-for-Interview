641382dc1f072454c5638f0bbb1ff03bc96b9d60
Niltiwari7 <nt465638@gmail.com>
2024-08-14 06:16:31 +0530

    good question on binary search

---
+++ Binary Search/Easy/medium/719. Find K-th Smallest Pair Distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int left = 0, right = nums[n-1] - nums[0];
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0, j = 0;
+            
+            for (int i = 0; i < n; i++) {
+                while (j < n && nums[j] - nums[i] <= mid) {
+                    j++;
+                }
+                count += j - i - 1; // count pairs with distance <= mid
+            }
+            
+            if (count < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+};
