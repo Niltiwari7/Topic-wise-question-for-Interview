ce80e6b378a60bba9082f603a0128d4b188934e8
Niltiwari7 <nt465638@gmail.com>
2024-08-31 05:49:58 +0530

    easy to understand code

--- graph/Djkastra/1514. Path with Maximum Probability.cpp
+++ graph/Djkastra/1514. Path with Maximum Probability.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& e, vector<double>& succProb, int start_node, int end_node) {
-        vector<vector<pair<int, double>>> adj(n);
-        for(int i = 0; i < e.size(); i++) {
-            adj[e[i][0]].push_back({e[i][1], succProb[i]});
-            adj[e[i][1]].push_back({e[i][0], succProb[i]});
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        vector<vector<pair<int,double>>>adj(n);
+        for(int i = 0;i<edges.size();i++){
+            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
         }
 
-        vector<double> dist(n, 0);
-        dist[start_node] = 1.0;
-
-        priority_queue<pair<double, int>> pq;
-        pq.push({1.0, start_node});
-
-        while(!pq.empty()) {
+        priority_queue<pair<double,int>>pq;
+        vector<double>p(n,0);
+        pq.push({1.0,start_node});
+        p[start_node] = 1.0;
+        while(!pq.empty()){
             double prob = pq.top().first;
             int node = pq.top().second;
             pq.pop();
-
-            if(node == end_node) {
+            if(node==end_node){
                 return prob;
             }
-
-            for(auto &it : adj[node]) {
-                int neighbor = it.first;
-                double edgeProb = it.second;
-
-                if(prob * edgeProb > dist[neighbor]) {
-                    dist[neighbor] = prob * edgeProb;
-                    pq.push({dist[neighbor], neighbor});
+            for(auto &it:adj[node]){
+                int adjNode = it.first;
+                double val = it.second;
+                if(p[node]*val>p[adjNode]){
+                    p[adjNode] = p[node]*val;
+                    pq.push({p[adjNode],adjNode});
                 }
             }
         }
-
-        return dist[end_node];
+        return 0;
     }
 };
