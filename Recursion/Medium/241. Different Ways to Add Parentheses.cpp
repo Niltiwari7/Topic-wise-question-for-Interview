d75a3f25bffa893f72744d8eedf034ea5f5942c3
Niltiwari7 <nt465638@gmail.com>
2024-09-19 05:47:52 +0530

    divide and conquer good question

---
+++ Recursion/Medium/241. Different Ways to Add Parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int perform(int x,int y,char op){
+        if(op=='+')return x+y;
+        if(op=='-') return x-y;
+        if(op=='*')return x*y;
+        return 0;
+    }
+    vector<int> diffWaysToCompute(string exp) {
+        vector<int>result;
+        bool isNumber = 1;
+        for(int i = 0;i<exp.size();i++){
+            if(!isdigit(exp[i])){
+                isNumber = 0;
+                vector<int>left = diffWaysToCompute(exp.substr(0,i));
+                vector<int>right = diffWaysToCompute(exp.substr(i+1));
+                for(int x:left){
+                    for(auto y:right){
+                        int val = perform(x,y,exp[i]);
+                        result.push_back(val);
+                    }
+                }
+            }
+        }
+        if(isNumber==1){
+            result.push_back(stoi(exp));
+        }
+        return result;
+    }
+};
