b6ce22bfcc6ba50e027eee0a0aeec1153aa93716
Niltiwari7 <nt465638@gmail.com>
2024-12-17 06:01:28 +0530

    Good Question on priority Queue

---
+++ Priority_Queue/Medium/2182. Construct String With Repeat Limit.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        unordered_map<char, int> mp;
+
+        for (char ch : s) {
+            mp[ch]++;
+        }
+
+        priority_queue<pair<char, int>> pq;
+        for (auto it : mp) {
+            pq.push({it.first, it.second});
+        }
+
+        string ans = "";
+        while (!pq.empty()) {
+            auto [ch, freq] = pq.top();
+            pq.pop();
+
+            int useCount = min(freq, repeatLimit); 
+
+            for (int i = 0; i < useCount; i++) {
+                ans += ch;
+            }
+
+            freq -= useCount; 
+            if (freq > 0) {
+                if (pq.empty()) {
+                    break;
+                }
+
+                auto [nextCh, nextFreq] = pq.top();
+                pq.pop();
+
+                ans += nextCh; 
+                nextFreq--;
+
+           
+                if (nextFreq > 0) {
+                    pq.push({nextCh, nextFreq});
+                }
+
+              
+                pq.push({ch, freq});
+            }
+        }
+
+        return ans;
+    }
+};
+
