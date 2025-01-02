66b197c961a999e0edeee416ac74874757a3fc59
Niltiwari7 <nt465638@gmail.com>
2025-01-02 06:09:10 +0530

    Application on prefix sum

---
+++ Prefix Sum/Medium/2559. Count Vowel Strings in Ranges.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool check(char c) {
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    }
+    vector<int> vowelStrings(vector<string>& words,
+                             vector<vector<int>>& queries) {
+        vector<int> v, ans;
+        v.push_back(0);
+
+        for (auto w : words) {
+            v.push_back(v.back() + (check(w[0]) && check(w.back())));
+        }
+        for (auto q : queries) {
+            ans.push_back(v[q[1] + 1] - v[q[0]]);
+        }
+        return ans;
+    }
+};
