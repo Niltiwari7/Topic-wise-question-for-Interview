2be4206de02feac7254e89866092aedb97163a80
Niltiwari7 <nt465638@gmail.com>
2025-01-08 05:28:16 +0530

    Updated yesterday potd solution

--- KMP/1408. String Matching in an Array.cpp
+++ KMP/1408. String Matching in an Array.cpp
@@ -22,3 +22,78 @@ public:
         return ans;
     }
 };
+
+
+// Using KMP algorithm
+
+class Solution {
+public:
+    vector<int> LPS(string s) {
+        int length = 0;
+        int n = s.size();
+        int i = 1;
+        vector<int> lps(n, 0);
+        while (i < n) {
+            if (s[i] == s[length]) {
+                length++;
+                lps[i] = length;
+                i++;
+            } else {
+                if (length != 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
+    bool searchPattern(string& txt, string& pattern) {
+        int n = txt.size();
+        int m = pattern.size();
+
+        if (m == 0) {
+            return true;
+        }
+
+        vector<int> lps = LPS(pattern);
+        int i = 0;
+        int j = 0;
+
+        while (i < n) {
+            if (txt[i] == pattern[j]) {
+                i++;
+                j++;
+            }
+            if (j == m) {
+                return true;
+            } else if (i < n && txt[i] != pattern[j]) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (searchPattern(words[j], words[i])) {
+                    ans.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
