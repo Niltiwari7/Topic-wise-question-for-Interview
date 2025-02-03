bb19c5519bffeb9bf04688313d88e4f9bc347a5e
Niltiwari7 <nt465638@gmail.com>
2025-02-03 06:23:10 +0530

    Optimal and brute force question updated

--- Array/Easy/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
+++ Array/Easy/3105. Longest Strictly Increasing or Strictly Decreasing Subarray.cpp
@@ -1,3 +1,6 @@
+// Brute force 
+// Tc = O(n^2)
+// sc = O(1)
 class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
@@ -21,6 +24,36 @@ public:
                 }
                 res  = max({res,cntInc,cntDec});
             }
+        }
+        return res;
+    }
+};
+
+// Optimal solution
+// Tc = O(n)
+// sc = O(1)
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1){
+            return 1;
+        }
+        int res = 1;
+        int cntInc = 1;
+        int cntDec = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < nums[i + 1]) {
+                cntInc++;
+                cntDec=1;
+            } else if (nums[i] > nums[i + 1]) {
+                cntDec++;
+                cntInc=1;
+            } else {
+                cntDec=1;
+                cntInc=1;
+            }
+            res = max({res,cntInc,cntDec});
         }
         return res;
     }
